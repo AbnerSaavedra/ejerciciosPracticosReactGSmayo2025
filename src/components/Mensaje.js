b83c241014a4cb83c6cc85be27a3d6ec8c2ee3a8
Abner Saavedra <ingenieroabnersaavedra@gmail.com>
2025-05-08 08:58:15 -0400

    Describiendo la UI ejemplo 1

---
+++ src/components/Mensaje.js
@@ -0,0 +1,16 @@
+
+
+export default function Mensaje(){
+    return (
+        <div>
+            <h1>
+                ¡Mi primer componente React!
+            </h1>
+            <p>
+                Este es un componente de función.
+            </p>
+        </div>
+    )
+}
+
+//export default Mensaje
